b3f240d4e26ae1e15d88a8336d6d6cd6e51e6f81
SaulNeri12 <saul.neri247498@potros.itson.edu.mx>
2024-12-01 00:44:43 -0600

    Se muestran los dos scrollpanes correctamente

--- Presentacion/src/main/java/com/equipo7/presentacion/gui/FrmFavoritos.java
+++ Presentacion/src/main/java/com/equipo7/presentacion/gui/FrmFavoritos.java
@@ -4,11 +4,13 @@
  */
 package com.equipo7.presentacion.gui;
 
+import com.equipo7.presentacion.gui.estilo.Estilo;
 import com.equipo7.presentacion.gui.paneles.AlbumPanel;
 import com.equipo7.presentacion.gui.paneles.ArtistaPanel;
 import com.equipo7.presentacion.gui.paneles.PanelResultadosFlexible;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JScrollBar;
@@ -27,9 +29,17 @@ public class FrmFavoritos extends javax.swing.JFrame {
     public FrmFavoritos() {
         initComponents();
 
-        this.resultadosArtistasPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        this.resultadosArtistasPanel.setLayout(new BoxLayout(this.resultadosArtistasPanel, BoxLayout.X_AXIS));
+        this.resultadosArtistasScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
         this.resultadosArtistasScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+        this.resultadosAlbumsScrollPane.setPreferredSize(new Dimension(1100, 250));
         
+        this.resultadosAlbumsPanel.setLayout(new BoxLayout(this.resultadosAlbumsPanel, BoxLayout.X_AXIS));
+        this.resultadosAlbumsScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        this.resultadosAlbumsScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+        this.resultadosAlbumsScrollPane.setPreferredSize(new Dimension(1100, 250));
+        
+        this.setResizable(false);
         this.prepararEstilo();
         this.cargarResultados();
     }
@@ -38,11 +48,29 @@ public class FrmFavoritos extends javax.swing.JFrame {
         for (int i = 0; i < 20; i++) {
             AlbumPanel pnl = new AlbumPanel();
             this.resultadosArtistasPanel.add(pnl);
+            this.resultadosArtistasPanel.add(Box.createRigidArea(new Dimension(10, 0))); // espacio
         }
-        
+
+        /*
+        this.resultadosArtistasPanel.revalidate();
         this.resultadosArtistasPanel.repaint();
         this.resultadosArtistasScrollPane.revalidate();
         this.resultadosArtistasScrollPane.repaint();
+*/
+        
+        for (int i = 0; i < 20; i++) {
+            AlbumPanel pnl = new AlbumPanel();
+            this.resultadosAlbumsPanel.add(pnl);
+        }
+        
+        /*
+        this.resultadosAlbumsPanel.revalidate();
+        this.resultadosAlbumsPanel.repaint();
+        this.resultadosAlbumsScrollPane.revalidate();
+        this.resultadosAlbumsScrollPane.repaint();*/
+        
+        this.revalidate();
+        this.repaint();
     }
 
     /**
@@ -67,9 +95,14 @@ public class FrmFavoritos extends javax.swing.JFrame {
         resultadosArtistasScrollPane = new javax.swing.JScrollPane();
         resultadosArtistasPanel = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        resultadosAlbumsScrollPane = new javax.swing.JScrollPane();
+        resultadosAlbumsPanel = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(20, 20, 20));
+        setMaximumSize(new java.awt.Dimension(1200, 999));
+        setMinimumSize(new java.awt.Dimension(1200, 999));
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
         jLabel1.setText("Resultados");
@@ -89,7 +122,7 @@ public class FrmFavoritos extends javax.swing.JFrame {
         resultadosArtistasPanel.setLayout(resultadosArtistasPanelLayout);
         resultadosArtistasPanelLayout.setHorizontalGroup(
             resultadosArtistasPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1133, Short.MAX_VALUE)
+            .addGap(0, 1141, Short.MAX_VALUE)
         );
         resultadosArtistasPanelLayout.setVerticalGroup(
             resultadosArtistasPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -99,7 +132,23 @@ public class FrmFavoritos extends javax.swing.JFrame {
         resultadosArtistasScrollPane.setViewportView(resultadosArtistasPanel);
 
         jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 20)); // NOI18N
-        jLabel2.setText("Artistas");
+        jLabel2.setText("Albums");
+
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 20)); // NOI18N
+        jLabel3.setText("Artistas");
+
+        javax.swing.GroupLayout resultadosAlbumsPanelLayout = new javax.swing.GroupLayout(resultadosAlbumsPanel);
+        resultadosAlbumsPanel.setLayout(resultadosAlbumsPanelLayout);
+        resultadosAlbumsPanelLayout.setHorizontalGroup(
+            resultadosAlbumsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1137, Short.MAX_VALUE)
+        );
+        resultadosAlbumsPanelLayout.setVerticalGroup(
+            resultadosAlbumsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 244, Short.MAX_VALUE)
+        );
+
+        resultadosAlbumsScrollPane.setViewportView(resultadosAlbumsPanel);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -112,11 +161,13 @@ public class FrmFavoritos extends javax.swing.JFrame {
                         .addComponent(volverBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 161, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(31, 31, 31)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 282, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(resultadosArtistasScrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, 1139, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(30, Short.MAX_VALUE))
+                            .addComponent(resultadosArtistasScrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(resultadosAlbumsScrollPane))))
+                .addContainerGap(26, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -126,10 +177,14 @@ public class FrmFavoritos extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(resultadosArtistasScrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(556, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(resultadosAlbumsScrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(288, Short.MAX_VALUE))
         );
 
         pack();
@@ -177,6 +232,9 @@ public class FrmFavoritos extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel resultadosAlbumsPanel;
+    private javax.swing.JScrollPane resultadosAlbumsScrollPane;
     private javax.swing.JPanel resultadosArtistasPanel;
     private javax.swing.JScrollPane resultadosArtistasScrollPane;
     private javax.swing.JButton volverBtn;
